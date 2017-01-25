8715c55e5299f9ce7e127d384d989303850c5e56
Ascold <ascold@ex.ua>
2017-01-25 21:24:38 +0200

    first commit

---
+++ source-js/main.js
@@ -0,0 +1,27 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon*/
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav clearfix") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav clearfix";
+    }
+}
+
+$(document).ready(function(){
+    $('#header-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        margin: 27,
+        nav: false,
+        dots: true
+    });
+    $('#about-us-carousel').owlCarousel({
+        loop: true,
+        margin: 62,
+        items: 5,
+        dots: false,
+        nav: true,
+        navText: ["",""]
+    });
+});
